43da9a32ecefa2d91d55b988338622d097cdc01d
FabianGuarascio <guarascio.fabiand@gmail.com>
2022-06-06 15:44:12 -0300

    Hecho el ruteo de dashboar y enganchado al approuting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardModule } from './dashboard/dashboard.module';
 import { LoginModule } from './login/login.module';
 
 const routes:Routes=[
   {
     path:"login",
     loadChildren:()=> import("./login/login.module").then(m=>LoginModule)
+  },
+  {
+    path:"dashboard",
+    loadChildren:()=> import("./dashboard/dashboard.module").then(m=>DashboardModule)
   },
   {
     path:"",
