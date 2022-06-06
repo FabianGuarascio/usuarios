e688daaeb6a132e1603d9506d4bff98aad662caa
FabianGuarascio <guarascio.fabiand@gmail.com>
2022-06-06 19:06:45 -0300

    agregado bootstrap y hecho la lista de contactos

--- src/app/dashboard/components/listado-contactos/listado-contactos.component.ts
+++ src/app/dashboard/components/listado-contactos/listado-contactos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactoInterface } from '../../contacto-interface';
 
 @Component({
   selector: 'app-listado-contactos',
@@ -7,6 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadoContactosComponent implements OnInit {
 
+  contactos:ContactoInterface[]=[
+    {
+      nombre:"Enrique",
+      apellido:"Garcia",
+      telefono:45716895,
+      email:"test@gmail",
+      pais:"Timbuktu",
+      ciudad:"Londres",
+      calle:"441 baker street"
+    },
+    {
+      nombre:"Jacinta",
+      apellido:"Garcia",
+      telefono:157401928,
+      email:"test2@gmail",
+      pais:"Argentina",
+      ciudad:"Cordoba",
+      calle:"San Martin 200"
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/dashboard/contacto-interface.ts
@@ -0,0 +1,9 @@
+export interface ContactoInterface {
+    nombre:string;
+    apellido:string;
+    telefono:number;
+    email:string;
+    pais:string;
+    ciudad:string;
+    calle:string;
+}
