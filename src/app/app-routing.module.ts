7e4a7dd774475d55b01d52ab5e2306feeeb46b63
FabianGuarascio <guarascio.fabiand@gmail.com>
2022-06-06 15:28:43 -0300

    creado el ruteo en Approuting para cargar el login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginModule } from './login/login.module';
 
 const routes:Routes=[
-  {}
+  {
+    path:"login",
+    loadChildren:()=> import("./login/login.module").then(m=>LoginModule)
+  },
+  {
+    path:"",
+    loadChildren:()=> import("./login/login.module").then(m=>LoginModule)
+  }
 ]
 
 @NgModule({
